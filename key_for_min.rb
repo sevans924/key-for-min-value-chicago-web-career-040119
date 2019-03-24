2dc5f582c977f0aab51e2773804ef33c9e38c57f
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 23:01:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+smallest_value = nil
+smallest_key = nil
+
+hash.each do |key, value|
+  if smallest_value = nil || value < smallest_value
+    smallest_value = value
+    smallest_key = key
+  end
+end
+smallest_key
 end
